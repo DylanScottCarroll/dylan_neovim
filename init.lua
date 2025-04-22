9e6a867a6f57b18cea900a89256f85e38747c5aa
Dylan Carroll <dyscarrollgmail.com>
2025-04-21 17:34:28 -0700

    Added files

---
+++ init.lua
@@ -0,0 +1,87 @@
+--
+-- Sane Defaults
+--
+
+-- Enable line numbers
+vim.opt.number = true
+
+-- Use spaces instead of tabs
+vim.opt.expandtab = true
+
+-- Set tab width to 4 spaces
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+
+-- Enable mouse support
+vim.opt.mouse = 'a'
+
+-- Enable syntax highlighting
+vim.cmd('syntax on')
+
+-- Set default encoding to UTF-8
+vim.opt.encoding = 'utf-8'
+
+-- Enable persistent undo
+vim.opt.undofile = true
+
+-- Case-insensitive search, unless capital letter or `/C` is used
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Incremental search
+vim.opt.incsearch = true
+
+-- Highlight search results
+vim.opt.hlsearch = true
+
+-- Show matching brackets
+vim.opt.showmatch = true
+
+-- Remove numbers of some files
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = {"markdown", "text"},
+  callback = function()
+    vim.wo.wrap = true
+  end
+})
+
+
+--
+-- Plugin Manager
+--
+
+require("config.lazy")
+
+--
+-- Customizations 
+--
+
+local map = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+
+-- Change delete to use the d register
+map("n", "d", '"dd', opts)   
+map("v", "d", '"dd', opts)   
+map("n", "dd", '"ddd', opts) 
+map("n", "D", '"dD', opts)  
+
+-- Change the clipboard to be the unnamed register
+vim.opt.clipboard = "unnamedplus"
+
+
+-- Alt Dragging!
+map('n', '<A-Up>', ':m .-2<CR>', opts)  -- Move the current line up by 1
+map('n', '<A-Down>', ':m .+1<CR>', opts) -- Move the current line down by 1
+
+map('v', '<A-Up>', ":move '<-2<CR>gv", opts)  -- Move selected lines up and reselect them
+map('v', '<A-Down>', ":move '>+1<CR>gv", opts)  -- Move selected lines down and reselect them
+
+-- Enter and Delete Being Nice in Normal mode
+map('n', '<Enter>', 'a<Enter><Esc>', opts)
+map('n', '<BS>', 'i<BS><Esc><Right>', opts)
+
+
+
+require('lsp_signature').on_attach()
+vim.o.mousemoveevent = true
+
---
+++ lua/plugins/spec.lua
@@ -0,0 +1,120 @@
+return { 
+   
+  -- TODO: Comments
+  { "folke/todo-comments.nvim", opts = {} },
+
+
+  {
+    "williamboman/mason.nvim",
+    build = ":masonupdate", -- optional: update mason registry on install
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = { "williamboman/mason.nvim" },
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",     -- lua
+          "pyright",    -- python
+          "rust_analyzer",  -- rust
+          "clangd",     -- c/c++
+        },
+        automatic_installation = true,
+      })
+    end,
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+  
+      -- Example: only attach `lua_ls` to Lua files
+      lspconfig.lua_ls.setup({
+        settings = {
+          Lua = {
+            diagnostics = { globals = { "vim" } },
+          },
+        },
+      })
+  
+      -- Example: only attach `pyright` to Python files
+      lspconfig.pyright.setup({})
+  
+      -- You can even lazy-load servers manually if needed,
+      -- but lspconfig will do it automatically when you open the right file.
+    end,
+  },
+
+  {
+    'stevearc/aerial.nvim',
+    opts = {},
+    -- Optional dependencies
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons"
+    },
+  },
+
+  {
+    "ray-x/lsp_signature.nvim",
+    event = "InsertEnter",
+    opts = {
+        bind = true,
+        hint_prefix = "->"
+    },
+  },
+
+  {
+    "soulis-1256/eagle.nvim",
+    opts = {
+      --override the default values found in config.lua
+    }
+  },
+
+
+  --{
+  --  "ms-jpq/coq_nvim",
+  --  branch = "coq", -- main plugin
+  --  dependencies = {
+  --    { "ms-jpq/coq.artifacts", branch = "artifacts" }, -- recommended sources
+  --    { "ms-jpq/coq.thirdparty", branch = "3p" },        -- optional: external sources
+  --  },
+  --  build = ":COQdeps", -- runs dependency setup on install
+  --  init = function()
+  --    -- Optional: disable automatic startup banner
+  --    vim.g.coq_settings = {
+  --      keymap = {
+  --          recommended = false,
+  --          pre_select = true,
+  --      },
+  --      auto_start = "shut-up", -- don't prompt to start
+  --      display = {
+  --        preview = {
+  --          border = "rounded",
+  --        },
+  --      },
+  --    }
+  --  end,
+  --  config = function()
+  --    -- Optional: use with lspconfig
+  --    local lspconfig = require("lspconfig")
+  --    local coq = require("coq")
+
+  --    -- Example LSP: Lua
+  --    lspconfig.lua_ls.setup(coq.lsp_ensure_capabilities({
+  --      settings = {
+  --        Lua = {
+  --          diagnostics = { globals = { "vim" } },
+  --        },
+  --      },
+  --    }))
+
+  --    -- Add your other LSPs here, wrapped with `coq.lsp_ensure_capabilities`
+  --  end,
+  --},
+}
+
